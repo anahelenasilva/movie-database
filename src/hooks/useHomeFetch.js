bafe39dac72077d6c69f9315dd87a671409f498a
Ana Helena <anahelenarp@hotmail.com>
2021-08-28 18:22:43 -0300

    create useHomeFetch custom hook

---
+++ src/hooks/useHomeFetch.js
@@ -0,0 +1,44 @@
+import { useState, useEffect } from 'react';
+
+import API from '../API';
+
+const inititalState = {
+  page: 0,
+  results: [],
+  total_pages: 0,
+  total_results: 0
+}
+
+export const useHomeFetch = () => {
+  const [state, setState] = useState(inititalState)
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState(false)
+
+  const fetchMovies = async (page, searchTerm = '') => {
+    try {
+      setError(false)
+      setLoading(true)
+
+      const movies = await API.fetchMovies(page, searchTerm)
+
+      setState(prev => ({
+        ...movies,
+        results: page > 1 ? [...prev.results, ...movies.results] : [...movies.results]
+      }))
+
+    } catch (error) {
+      setError(true)
+      setLoading(false)
+    }
+
+    setLoading(false)
+  }
+
+  useEffect(() => {
+
+    fetchMovies(1)
+
+  }, [])//only when mount
+
+  return { state, loading, error }
+}
