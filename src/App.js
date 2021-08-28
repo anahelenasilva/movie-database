b9e42ef3c0a0b7955d53fed03eb7cb44f6ce3c35
Ana Helena <anahelenarp@hotmail.com>
2021-08-28 10:22:40 -0300

    Initital code

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Header from './components/Header';
+import { GlobalStyle } from './GlobalStyle';
+
+const App = () => (
+  <div>
+    <Header />
+    <GlobalStyle />
+  </div>
+);
+
+export default App;
