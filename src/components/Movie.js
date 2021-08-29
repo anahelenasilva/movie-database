b36d0b2ded1c51bed153beeab20d20e72588b3e6
Ana Helena <anahelenarp@hotmail.com>
2021-08-29 19:37:40 -0300

    feat: create Actor component

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -12,6 +12,7 @@ import { useMovieFetch } from '../hooks/useMovieFetch';
 
 import NoImage from '../images/no_image.jpg'
 import MovieInfoBar from './MovieInfoBar';
+import Actor from './Actor';
 
 const Movie = () => {
 
@@ -25,7 +26,24 @@ const Movie = () => {
     <>
       <BreadCrumb movieTitle={movie.original_title} />
       <MovieInfo movie={movie} />
-      <MovieInfoBar time={movie.runtime} budget={movie.budget} revenue={movie.revenue} />
+      <MovieInfoBar
+        time={movie.runtime}
+        budget={movie.budget}
+        revenue={movie.revenue} />
+
+      <Grid header='Actors'>
+        {movie.actors.map(actor => (
+          <Actor
+            key={actor.credit_id}
+            name={actor.name}
+            character={actor.character}
+            imageUrl={
+              actor.profile_path ?
+                `${IMAGE_BASE_URL}${POSTER_SIZE}${actor.profile_path}`
+                : NoImage
+            } />
+        ))}
+      </Grid>
     </>
   )
 }
