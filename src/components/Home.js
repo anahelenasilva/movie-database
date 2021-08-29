b5c55c76b2c3e6f2581547d16a2da0c6c1865649
Ana Helena <anahelenarp@hotmail.com>
2021-08-29 08:52:41 -0300

    feat: create hero image component

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,6 +4,8 @@ import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from '../config'
 
 import NoImage from '../images/no_image.jpg'
 
+import HeroImage from './HeroImage';
+
 import { useHomeFetch } from '../hooks/useHomeFetch'
 
 const Home = () => {
@@ -12,7 +14,17 @@ const Home = () => {
 
   console.log(state)
 
-  return <div>Home Page</div>
+  return (
+    <>
+      {state.results[0] && //short circuit
+        <HeroImage
+          image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${state.results[0].backdrop_path}`}
+          title={state.results[0].original_title}
+          text={state.results[0].overview}
+        />
+      }
+    </>
+  )
 }
 
 export default Home
