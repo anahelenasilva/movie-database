3f8c3bfc8f7c94d614f8ae55e5e2af972ee223e4
Ana Helena <anahelenarp@hotmail.com>
2021-09-01 08:25:00 -0300

    refactor: change SearchBar component to a class component

--- src/components/SearchBar/index.js
+++ src/components/SearchBar/index.js
@@ -1,42 +1,48 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 import searchIcon from '../../images/search-icon.svg'
 
 import { Content, Wrapper } from './SearchBar.styles'
 
-const SearchBar = ({ setSearchTerm }) => {
+class SearchBar extends Component {
 
-  const [state, setState] = useState('')
-  const initial = useRef(true)
+  state = {
+    value: '',
+  }
 
-  useEffect(() => {
+  timeout = null
 
-    if (initial.current) {
-      initial.current = false
-      return
+  componentDidUpdate(_prevProps, prevState) {
+    if (prevState.value !== this.state.value) {
+      const { setSearchTerm } = this.props
+
+      clearTimeout(this.timeout)
+
+      this.timeout = setTimeout(() => {
+        const { value } = this.state
+        setSearchTerm(value)
+      }, 500)
     }
+  }
 
-    const timer = setTimeout(() => {
-      setSearchTerm(state)
-    }, 500)
+  render() {
+    const { value } = this.state
+    return <Wrapper>
+      <Content>
 
-    return () => clearTimeout(timer)
-  }, [setSearchTerm, state])
+        <img src={searchIcon} alt='A magnifying glass search icon' />
+        <input
+          type='text'
+          placeholder='Search Movie'
+          onChange={event => this.setState({ value: event.currentTarget.value })}
+          value={value}
+        />
 
-  return <Wrapper>
-    <Content>
+      </Content>
+    </Wrapper >
+  }
 
-      <img src={searchIcon} alt='A magnifying glass search icon' />
-      <input
-        type='text'
-        placeholder='Search Movie'
-        onChange={event => setState(event.currentTarget.value)}
-        value={state}
-      />
-
-    </Content>
-  </Wrapper>
 }
 
 SearchBar.propTypes = {
